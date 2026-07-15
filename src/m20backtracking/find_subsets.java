afcc3f503d77bd67eaa446dc2f8b89c91ef3118e
Harshit <harshitvats237@gmail.com>
2026-07-15 16:40:34 +0530

    NEW FILE

--- src/m20backtracking/find_subsets.java
+++ src/m20backtracking/find_subsets.java
@@ -1,7 +1,26 @@
 package m20backtracking;
 
 public class find_subsets {
-    public static void main(String[] args) {
 
+    public static void subsets(String str,String ans,int i){
+        if(i==str.length()){
+            if (ans.length()==0){
+                System.out.print("null");
+                return;
+            }else{
+                System.out.println(ans);
+                return;
+            }
+
+        }
+        // recursion ka kaam
+        // yes
+        subsets(str,ans+str.charAt(i),i+1);
+        // no
+        subsets(str,ans,i+1);
+    }
+    public static void main(String[] args) {
+String str="abc";
+subsets(str,"",0);
     }
 }
